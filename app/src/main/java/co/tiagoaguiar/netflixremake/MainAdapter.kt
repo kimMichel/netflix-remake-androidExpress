60f8651b9a708a9efbd7638a83b26c636da6e23b
kimMichel <kmichel.jae@gmail.com>
2022-08-02 20:52:52 -0300

    work with image and DimensionRatio

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainAdapter.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainAdapter.kt
@@ -3,6 +3,7 @@ package co.tiagoaguiar.netflixremake
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Movie
@@ -26,8 +27,8 @@ class MainAdapter(private val movies: List<Movie>) :
 
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(movie: Movie) {
-            val txtTest: TextView = itemView.findViewById(R.id.txt_test)
-            txtTest.text = movie.coverUrl
+            val imageCover: ImageView = itemView.findViewById(R.id.img_cover)
+            imageCover.setImageResource(movie.coverUrl)
         }
     }
 
