86e89f222861d1cb231637ef3f5c5d6b39938f73
kimMichel <kmichel.jae@gmail.com>
2022-08-03 21:12:00 -0300

    Add imageview with shadow in movie desc

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -1,8 +1,12 @@
 package co.tiagoaguiar.netflixremake
 
+import android.content.Context
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,20 @@ class MovieActivity : AppCompatActivity() {
         // Para fazer com que nosso botão de voltar apareça
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Geralmente utilizamos o ContextCompat (e damos preferência) quando trabalhamos com desenháveis.
+        // busquei o desenhavel (layer-list)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+
+        // busquei o filme que eu quero
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+
+        // atribui a este layer-list o novo filme
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+
+        // set no imageview
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
 
     }
 }
