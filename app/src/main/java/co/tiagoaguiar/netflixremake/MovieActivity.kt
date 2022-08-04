d107392c24f346bce56cf86f857a310b61950c57
kimMichel <kmichel.jae@gmail.com>
2022-08-04 00:57:59 -0300

    Finish prototype movie desc UI

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -5,13 +5,35 @@ import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
+        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
+        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
+        val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        txtTitle.text = "Batman Begins"
+        txtDesc.text = "Essa é a descrição do filme do Batman"
+        txtCast.text = getString(R.string.cast, "Christian Bale, Anne Hattaway, Heath Ledger")
+
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 15) {
+            val movie = Movie(R.drawable.movie)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
 
         // Para fazer com que a toolbar apareça
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
@@ -20,6 +42,8 @@ class MovieActivity : AppCompatActivity() {
         // Para fazer com que nosso botão de voltar apareça
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Remove o titulo da toolbar
+        supportActionBar?.title = null
 
         // Geralmente utilizamos o ContextCompat (e damos preferência) quando trabalhamos com desenháveis.
         // busquei o desenhavel (layer-list)
