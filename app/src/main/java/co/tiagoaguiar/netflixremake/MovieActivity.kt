9141374b028d328c577e8c03e991f2d359a8f865
kimMichel <kmichel.jae@gmail.com>
2022-08-10 23:56:41 -0300

    Get movie detail and similar movies

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Movie
+import co.tiagoaguiar.netflixremake.model.MovieDetail
+import co.tiagoaguiar.netflixremake.util.MovieTask
+import java.lang.IllegalStateException
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), MovieTask.Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
@@ -22,6 +27,12 @@ class MovieActivity : AppCompatActivity() {
         val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
         val txtCast: TextView = findViewById(R.id.movie_txt_cast)
         val rv: RecyclerView = findViewById(R.id.movie_rv_similar)
+
+        val id = intent?.getIntExtra("id", 0) ?: throw IllegalStateException("ID não foi encontrado!")
+
+        val url = "https://api.tiagoaguiar.co/netflixapp/movie/$id?apiKey=4bab9164-66bd-479d-8fab-47b7579e6563"
+
+        MovieTask(this).execute(url)
 
         txtTitle.text = "Batman Begins"
         txtDesc.text = "Essa é a descrição do filme do Batman"
@@ -57,6 +68,18 @@ class MovieActivity : AppCompatActivity() {
         val coverImg: ImageView = findViewById(R.id.movie_img)
         coverImg.setImageDrawable(layerDrawable)
 
+    }
+
+    override fun onPreExecute() {
+
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResult(movieDetail: MovieDetail) {
+        Log.i("Test", movieDetail.toString())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
